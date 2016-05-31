c8fc5e3cf06950cbdbdf769189be613b8003aac4
佐藤優衣 <yui.chocolat1538@gmail.com>
2016-05-31 19:33:09 +0900

    Initial Commit

---
+++ kanjiGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kanjiGame
+//
+//  Created by 佐藤優衣 on 2016/05/31.
+//  Copyright © 2016年 Yui Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
